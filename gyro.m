37a0952fef36edb303ba3db3ade0b5e738b1181b
apan64 <andrew.pan@students.olin.edu>
2015-11-13 11:57:23 -0500

    basic variables

--- gyro.m
+++ gyro.m
@@ -1,3 +1,7 @@
 wheelMass = 5; %kg
 wheelRadius = .5; %m
-wheelInertia = .25 * wheelMass * wheelRadius^2; %1/4 * mr^2
+wheelInertia = .25 * wheelMass * wheelRadius^2; %I = 1/4 * mr^2
+
+platformMass = 10; %kg
+platformRadius = .75; %m
+platformInertia = .25 * platformMass * platformRadius^2; %I = 1/4 * mr^2
