a3d73ade0839476a03ac4b7d202592a0fb1178bb
apan64 <andrew.pan@students.olin.edu>
2015-11-13 12:27:20 -0500

    momentum

--- gyro.m
+++ gyro.m
@@ -5,3 +5,10 @@ wheelInertia = .25 * wheelMass * wheelRadius^2; %I = 1/4 * mr^2
 platformMass = 10; %kg
 platformRadius = .75; %m
 platformInertia = .25 * platformMass * platformRadius^2; %I = 1/4 * mr^2
+
+%initial speeds
+wheelSpeed = 2; %radians/second
+platformSpeed = 0; %radians/second
+
+wheelMomentum = wheelInertia * wheelSpeed; %angular momentum
+platformMomentum = platformInertia * platformSpeed; %angular momentum
