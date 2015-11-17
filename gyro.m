4dc63206978c55f4018008cd02b552fd372cfdeb
apan64 <andrew.pan@students.olin.edu>
2015-11-17 18:00:59 -0500

    simple implementation model

--- gyro.m
+++ gyro.m
@@ -6,9 +6,24 @@ platformMass = 10; %kg
 platformRadius = .75; %m
 platformInertia = .25 * platformMass * platformRadius^2; %I = 1/4 * mr^2
 
-%initial speeds
+%initials
 wheelSpeed = 2; %radians/second
 platformSpeed = 0; %radians/second
+angle = 0;
 
 wheelMomentum = wheelInertia * wheelSpeed; %angular momentum
-platformMomentum = platformInertia * platformSpeed; %angular momentum
+wheelMomentumX = wheelInertia * wheelSpeed * sind(angle);
+wheelMomentumY = wheelInertia * wheelSpeed * cosd(angle);
+platformMomentum = platformInertia * platformSpeed; %angular momentum, only applies in one direction
+
+platformSpeeds = zeros(1, 90);
+
+for n = 1:90 %making wheel tilt 1 degree per loop
+    angle = n;
+    wheelMomentumX = wheelInertia * wheelSpeed * sind(angle);
+    wheelMomentumY = wheelInertia * wheelSpeed * cosd(angle);
+    platformSpeeds(n) = (platformMomentum + wheelMomentumX) / platformInertia;
+end
+plot(platformSpeeds);
+xlabel('Angle of Wheel to Vertical (degrees)');
+ylabel('Speed of Platform (radians/second)');
