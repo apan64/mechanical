673be33d34cbe95c11fcc9dbe5a715c95e5b61b8
apan64 <andrew.pan@students.olin.edu>
2015-12-03 11:49:01 -0500

    comments and stuff

--- ride.m
+++ ride.m
@@ -1,34 +1,31 @@
 function stuff = ride()
-    rideRPM = 0;
+    rideRPM = 0; %revolutions/minute
     rideSpeedRad = 0; %rad/s
     gravity = 9.81; %m/s^2
     rideRadius = 15.25; %m
     personMass = 80.7; %kg, americans are fat
     angle = 0;
     
-    rideSpeed = rideSpeedRad * rideRadius;
+    rideSpeed = rideSpeedRad * rideRadius; %calculating speed along the edge of the ride, m/s
     
-    bottoms = zeros(120, 80);
-    tops = zeros(120, 80);
+    bottoms = zeros(120, 80); %matrix for speeds at the bottom of the ride
+    tops = zeros(120, 80); %matrix for speeds at the top of the ride
     
-    increment = 1;
+    increment = 1; %amount to increase speed
     
     for r = 1:120
-        rideRPM = rideRPM + increment;
+        rideRPM = rideRPM + increment; %increment rpm
         rideSpeedRad = rideRPM / 30; %1 rpm = 1/30 rad/s
-        rideSpeed = rideSpeedRad * rideRadius;
-        gForce = rideSpeed^2 / rideRadius;
+        rideSpeed = rideSpeedRad * rideRadius; %recalculate speed
+        centrifugalA = rideSpeed^2 / rideRadius; %calculate centrifugal acceleration
         for c = 1:80
-            bottoms(r, c) = gForce + (gravity * sin(c*pi/180));
-            tops(r, c) = gForce - (gravity * sin(c*pi/180));
+            bottoms(r, c) = centrifugalA + (gravity * sin(c*pi/180)); %calculate total accelerations at bottom of ride
+            tops(r, c) = centrifugalA - (gravity * sin(c*pi/180)); %calculate total accelerations at top of ride
         end
     end
-    
-%     plot(bottoms./gravity);
-%     hold on;
-%     plot(tops./gravity);
-    h = pcolor(bottoms./gravity);
-    colormap(cool);
+
+    h = pcolor(bottoms./gravity); %heatmap
+    colormap(cool); %cool colors
     colorbar;
     axis([1, 80, 1, 120]);
     set(h, 'EdgeColor', 'none');
@@ -36,5 +33,5 @@ function stuff = ride()
     ylabel('Speed (RPM)');
     title('Heat Map of Maximum g-force');
     hold on;
-    contour(bottoms./gravity, [4, 17], 'LineColor', [0, 0, 0]);
+    contour(bottoms./gravity, [4, 17], 'LineColor', [0, 0, 0]); %contours to display g-force values on heatmap
 end
