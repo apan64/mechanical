b30c7c94c769495ccf8aaa12f43d090e6e681247
apan64 <andrew.pan@students.olin.edu>
2015-12-03 11:31:44 -0500

    rpm instead of rad/s

--- ride.m
+++ ride.m
@@ -1,4 +1,5 @@
 function stuff = ride()
+    rideRPM = 0;
     rideSpeedRad = 0; %rad/s
     gravity = 9.81; %m/s^2
     rideRadius = 15.25; %m
@@ -7,13 +8,14 @@ function stuff = ride()
     
     rideSpeed = rideSpeedRad * rideRadius;
     
-    bottoms = zeros(60, 80);
-    tops = zeros(60, 80);
+    bottoms = zeros(120, 80);
+    tops = zeros(120, 80);
     
-    increment = .1;
+    increment = 1;
     
-    for r = 1:60
-        rideSpeedRad = rideSpeedRad + increment;
+    for r = 1:120
+        rideRPM = rideRPM + increment;
+        rideSpeedRad = rideRPM / 30; %1 rpm = 1/30 rad/s
         rideSpeed = rideSpeedRad * rideRadius;
         gForce = rideSpeed^2 / rideRadius;
         for c = 1:80
@@ -27,10 +29,10 @@ function stuff = ride()
 %     plot(tops./gravity);
     h = pcolor(bottoms./gravity);
     colorbar;
-    axis([1, 80, 1, 60]);
+    axis([1, 80, 1, 120]);
     set(h, 'EdgeColor', 'none');
     xlabel('Angle (degrees)');
-    ylabel('Speed (radians/10 seconds)');
+    ylabel('Speed (RPM)');
     title('Heat Map of Maximum g-force');
     hold on;
     contour(bottoms./gravity, [17, 46.2], 'LineColor', [0, 0, 0]);
